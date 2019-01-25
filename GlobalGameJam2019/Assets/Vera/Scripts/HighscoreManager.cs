0e38dabd339a3433b58baf6c5d03a16bf2898f7f
VeraGerritse <anna.vera.gerritse@gmail.com>
2019-01-25 23:44:32 +0100

    hiiiiighscore

---
+++ GlobalGameJam2019/Assets/Vera/Scripts/HighscoreManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class HighscoreManager : MonoBehaviour
+{
+    public static HighscoreManager instance;
+    private string tempName;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void Save(Highscore tS)
+    {
+        SaveScore(tS);
+    }
+
+    [KeyCommand(KeyCode.L, PressType.KeyPressType.Down)]
+    void Load()
+    {
+        Highscore test = LoadSave();
+        print(test.score);
+    }
+
+    private void SaveScore(Highscore tS)
+    {
+        ByteSave toSave = new ByteSave();
+        toSave.save = ObjectToByteArray(tS);
+        var serializer = new XmlSerializer(typeof(ByteSave));
+        using (var stream = new System.IO.FileStream(Application.persistentDataPath + "/SavedGame_" + tempName + ".xml", FileMode.Create))
+        {
+            serializer.Serialize(stream, toSave);
+        }
+    }
+
+
+    private Highscore LoadSave()
+    {
+        var serializer = new XmlSerializer(typeof(ByteSave));
+        using (var stream = new System.IO.FileStream(Application.persistentDataPath + "/SavedGame_" + tempName + ".xml", FileMode.Open))
+        {
+            ByteSave save = serializer.Deserialize(stream) as ByteSave;
+            Highscore score = (Highscore)ByteArrayToObject(save.save);
+            return score;
+        }
+    }
+
+
+    private byte[] ObjectToByteArray(object obj)
+    {
+        if (obj == null)
+        {
+            return null;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        using (MemoryStream ms = new MemoryStream())
+        {
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+    }
+
+    private object ByteArrayToObject(byte[] bytes)
+    {
+        MemoryStream memStream = new MemoryStream();
+        BinaryFormatter binForm = new BinaryFormatter();
+        memStream.Write(bytes, 0, bytes.Length);
+        memStream.Seek(0, SeekOrigin.Begin);
+        object obj = binForm.Deserialize(memStream);
+
+        return obj;
+    }
+}
---
+++ GlobalGameJam2019/Assets/Vera/Scripts/NewHighscore.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewHighscore : MonoBehaviour
+{
+    [SerializeField] private float timeForLevel;
+    [SerializeField] private float maxScore;
+    [SerializeField] private float decrement;
+
+
+    [SerializeField] private float timeLeft;
+    [SerializeField] private float time;
+    [SerializeField] private int seconds;
+    [SerializeField] private int minutes;
+
+
+    [KeyCommand(KeyCode.V, PressType.KeyPressType.Down)]
+    private void SS()
+    {
+        StartScore(300, 5000);
+    }
+
+    private void StartScore(float _timeForLevel, float _maxScore)
+    {
+        timeForLevel = _timeForLevel;
+        maxScore = _maxScore;
+        decrement = maxScore / timeForLevel;
+        timeLeft = timeForLevel;
+    }
+
+    private void Update()
+    {
+        if(timeLeft > 0)
+        {
+            UpdateTime();
+        }
+    }
+
+    private void UpdateTime()
+    {
+        time += Time.deltaTime;
+        if(time > 1)
+        {
+            timeLeft--;
+            time--;
+        }
+        SecondMinute();
+    }
+
+    private void SecondMinute()
+    {
+        minutes =(int) Mathf.Floor(timeLeft / 60);
+        seconds = (int)timeLeft % 60;
+    }
+
+
+    private int GetScore()
+    {
+        int score = Mathf.RoundToInt(timeLeft * decrement);
+        return score;
+    }
+
+    [KeyCommand(KeyCode.S, PressType.KeyPressType.Down)]
+    private void Finished()
+    {
+        Highscore newHighscore = new Highscore();
+        newHighscore.score = GetScore();
+        HighscoreManager.instance.Save(newHighscore);
+    }
+}
