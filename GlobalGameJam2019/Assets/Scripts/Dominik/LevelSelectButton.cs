79ab7dc70ff0dc35d44ddb25afe0bdcd0a146dcb
Dominik Beens <dominik.beens@hotmail.nl>
2019-01-25 22:43:50 +0100

    menu stuff

---
+++ GlobalGameJam2019/Assets/Scripts/Dominik/LevelSelectButton.cs
@@ -0,0 +1,36 @@
+﻿using DB.MenuPack;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour
+{
+
+    private int sceneToLoad;
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        sceneToLoad = transform.GetSiblingIndex() + 1;
+        button.onClick.AddListener(() => LoadLevel());
+    }
+
+    private void OnValidate()
+    {
+        sceneToLoad = transform.GetSiblingIndex() + 1;
+        GetComponentInChildren<TextMeshProUGUI>().text = $"Level: {sceneToLoad}";
+        transform.name = $"LevelSelectButton L{sceneToLoad}";
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.instance.LoadScene(sceneToLoad, false);
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(() => LoadLevel());
+    }
+}
---
+++ GlobalGameJam2019/Assets/Scripts/Dominik/LevelSelectButtonConnector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectButtonConnector : MonoBehaviour
+{
+
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private Transform levelSelectButtonHolder;
+
+    private void Start()
+    {
+        CreateConnection();
+    }
+
+    private void OnValidate()
+    {
+        CreateConnection();
+    }
+    
+    [ContextMenu("CreateConnection")]
+    private void CreateConnection()
+    {
+        if (!line || !levelSelectButtonHolder)
+        {
+            return;
+        }
+
+        Camera mainCam = Camera.main;
+        List<Vector3> buttonWorldPos = new List<Vector3>();
+
+        foreach (Transform child in levelSelectButtonHolder)
+        {
+            Vector3 worldPos = mainCam.ScreenToWorldPoint(new Vector3(child.transform.position.x, child.transform.position.y, mainCam.nearClipPlane));
+            buttonWorldPos.Add(worldPos);
+        }
+
+        line.positionCount = buttonWorldPos.Count;
+        line.SetPositions(buttonWorldPos.ToArray());
+    }
+}
