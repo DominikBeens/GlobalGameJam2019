ed9a17f60eae64d096742c71f3dfab2ea330bbb5
han hakkenberg <han.hakkenberg@gmail.com>
2019-01-26 09:43:41 +0100

    Is That A Chicken

--- GlobalGameJam2019/Assets/[Individual]/Han/BirdMovement.cs
+++ GlobalGameJam2019/Assets/[Individual]/Han/BirdMovement.cs
@@ -8,34 +8,49 @@ public class BirdMovement : MonoBehaviour
     [SerializeField] int wingSpeed;
     [SerializeField] int rotationAmount;
     [SerializeField] Transform myamera;
+    [SerializeField] float stabelizeSpeed;
 
     void Awake(){
         myRidgidbody = GetComponent<Rigidbody2D>();
     }
 
-    Vector2Int newMovementAxis;
 
     void Update()
     {
         myamera.position = new Vector3(transform.position.x,transform.position.y,-10);
-        newMovementAxis = MovementAxis();
 
-        myRidgidbody.AddTorque((newMovementAxis.x - newMovementAxis.y) * rotationAmount * Time.deltaTime);
-        myRidgidbody.AddForce(transform.up * (newMovementAxis.x + newMovementAxis.y) * wingSpeed,ForceMode2D.Impulse);
+        MovementUpdate();
     }
 
-    Vector2Int MovementAxis(){
+    Vector2Int newMovementAxis;
+
+    void MovementUpdate(){
+        if(MovementAxis()){
+            myRidgidbody.AddTorque((newMovementAxis.x - newMovementAxis.y) * rotationAmount * Time.deltaTime);
+            myRidgidbody.AddForce(transform.up * (newMovementAxis.x + newMovementAxis.y) * wingSpeed,ForceMode2D.Impulse);
+        }
+        else if(myRidgidbody.velocity.x < 0.1 && myRidgidbody.velocity.x > -0.1 && myRidgidbody.velocity.y < 0.1 && myRidgidbody.velocity.y > -0.1){
+            transform.localEulerAngles = new Vector3(0,0,Mathf.LerpAngle(transform.localEulerAngles.z,0,stabelizeSpeed * Time.deltaTime));
+        }
+    }
+
+    bool MovementAxis(){
         if(Input.GetButtonDown("Right Wing")){
             if(Input.GetButtonDown("Left Wing")){
-                return(new Vector2Int(1,1));
+                newMovementAxis = new Vector2Int(1,1);
             }
 
-            return (new Vector2Int(0,1));
+             newMovementAxis = new Vector2Int(0,1);
+                return true;
+
         }
         else if(Input.GetButtonDown("Left Wing")){
-            return (new Vector2Int(1,0));
+             newMovementAxis = new Vector2Int(1,0);
+                return true;
         }
 
-        return (new Vector2Int(0,0));
+         newMovementAxis = new Vector2Int(0,0);
+                return false;
+
     }
 }
