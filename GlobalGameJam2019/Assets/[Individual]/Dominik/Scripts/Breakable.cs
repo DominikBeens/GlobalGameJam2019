03821fede0150b259387f53efe01734e1883387d
Dominik Beens <dominik.beens@hotmail.nl>
2019-01-26 16:59:26 +0100

    added breakable, tweaked levelmanager

---
+++ GlobalGameJam2019/Assets/[Individual]/Dominik/Scripts/Breakable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+
+    private Collider2D col;
+
+    [SerializeField] private float requiredBreakForce;
+
+    private void Awake()
+    {
+        col = GetComponentInChildren<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.gameObject.layer == 9)
+        {
+            if (Mathf.Abs(collision.relativeVelocity.x * collision.relativeVelocity.y) > requiredBreakForce)
+            {
+                col.enabled = false;
+            }
+        }
+    }
+}
