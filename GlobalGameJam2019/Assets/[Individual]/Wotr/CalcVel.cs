c31a5b973f0330ab356903fe3905d15ae812c3bc
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2019-01-26 21:07:20 +0100

    epic programming

---
+++ GlobalGameJam2019/Assets/[Individual]/Wotr/CalcVel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalcVel : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Animator myanim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myanim.SetFloat("DownwardVel", -rb.velocity.y);
+    }
+}
