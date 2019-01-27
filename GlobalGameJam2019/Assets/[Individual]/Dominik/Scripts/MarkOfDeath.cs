b020ad4d64f18b3393e95b87c97b4a884e797a84
Dominik Beens <dominik.beens@hotmail.nl>
2019-01-27 10:26:24 +0100

    mark, tweaks, level 1 edit

---
+++ GlobalGameJam2019/Assets/[Individual]/Dominik/Scripts/MarkOfDeath.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class MarkOfDeath : MonoBehaviour
+{
+
+    private GameObject activeMark;
+    private float markDuration;
+
+    [SerializeField] private GameObject markPrefab;
+    [SerializeField] private Transform markSpawn;
+    [SerializeField] private float triggerDuration = 3f;
+
+    [Space]
+
+    [SerializeField] private GameObject markTriggerCanvas;
+
+    private void Awake()
+    {
+        markTriggerCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (activeMark)
+        {
+            markDuration += Time.deltaTime;
+
+            if (markDuration >= triggerDuration)
+            {
+                TriggerMark();
+            }
+        }
+    }
+
+    [KeyCommand(KeyCode.G, PressType.KeyPressType.Down)]
+    private void AddMark()
+    {
+        if (activeMark)
+        {
+            DestroyMark();
+        }
+
+        activeMark = Instantiate(markPrefab, markSpawn.position, Quaternion.identity);
+    }
+
+    private void TriggerMark()
+    {
+        markTriggerCanvas.SetActive(true);
+    }
+
+    private void DestroyMark()
+    {
+        Destroy(activeMark);
+        activeMark = null;
+    }
+
+    public void AnimationEventKillBurb()
+    {
+        GameManager.instance.SpawnNewBird();
+    }
+}
---
+++ GlobalGameJam2019/Assets/[Individual]/Dominik/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField] private float rotateSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.right * (Time.deltaTime * rotateSpeed));
+    }
+}
