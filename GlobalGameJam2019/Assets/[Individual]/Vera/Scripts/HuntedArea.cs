fb5d0fd2bbcd334f59e8c2d0f544636eab618cd5
VeraGerritse <anna.vera.gerritse@gmail.com>
2019-01-27 10:59:10 +0100

    Hunt Area

---
+++ GlobalGameJam2019/Assets/[Individual]/Vera/Scripts/HuntedArea.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HuntedArea : MonoBehaviour
+{
+    [Header("doesnt have to be filled")]
+    [SerializeField] private Canvas turtorial;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+            if (GetComponentInChildren<MarkOfDeath>() != null)
+            {
+                GetComponentInChildren<MarkOfDeath>().AddMark();
+            }
+
+            if(turtorial != null)
+            {
+                turtorial.transform.parent = collision.transform.root.GetComponentInChildren<Camera>().gameObject.transform;
+                turtorial.transform.position = collision.transform.position;
+                turtorial.enabled = true;
+                turtorial.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 200);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            if (GetComponentInChildren<MarkOfDeath>() != null)
+            {
+                GetComponentInChildren<MarkOfDeath>().DestroyMark();
+            }
+            if (turtorial != null)
+            {
+                turtorial.transform.parent = null;
+                turtorial.enabled = false;
+            }
+        }
+    }
+}
